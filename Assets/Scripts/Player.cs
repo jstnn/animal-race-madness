034c18161af5e9ce9e354912ff89c0b00c36e4fd
Nicolas Justiniano <n.justiniano@gmail.com>
2017-12-28 20:46:58 -0300

    json with animations names by character type

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+
+public class Player : MonoBehaviour {
+
+	public float velocity = 0f;
+	public float experience = 0f;
+	public string playerType;
+	public string uuid;
+	public string idleName = "idle";
+	public string walkName = "walk";
+	public string runName = "run";
+	public bool mainPlayer = false;
+
+	// Use this for initialization
+	void Start () {
+		
+		string animationsData = Read("animations");
+		var data = JSON.Parse(animationsData);
+		var animations = data ["animations"];
+		foreach(System.Collections.Generic.KeyValuePair<string,SimpleJSON.JSONNode> kvp in animations) {
+			// Debug.Log(playerType+"+@@@@@@+"+kvp.Value ["type"].Value);
+			if ( playerType == kvp.Value ["type"].Value ) {
+				idleName = kvp.Value ["idle"].Value;
+				walkName = kvp.Value ["walk"].Value;
+				runName = kvp.Value ["run"].Value;
+			}
+
+		}
+		this.transform.localScale = new Vector3 (2, 2, 2);
+	}
+			
+
+
+
+
+	void Update() {
+		this.GetComponent<Animation> ().Play (idleName);
+		if (mainPlayer==true) {
+			if (Input.GetKeyDown (KeyCode.Space)) {
+
+				this.GetComponent<Animation> ().Play (runName);
+
+
+				Vector3 position = this.transform.position;
+				position.z++;
+				this.transform.position = position;
+
+
+			} else {
+				this.GetComponent<Animation> ().Play (idleName);
+			}
+		}
+
+	}
+
+
+
+	public static string Read(string filename) {
+		//Load the text file using Reources.Load
+		TextAsset theTextFile = Resources.Load<TextAsset>(filename);
+
+		//There's a text file named filename, lets get it's contents and return it
+		if (theTextFile != null) {
+			return theTextFile.text;
+		} else {
+			//There's no file, return an empty string.
+			return "";
+		}
+	}
+
+}
---
+++ Assets/Scripts/RaceManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
+
+public class RaceManager : MonoBehaviour {
+	public List<GameObject> players = new List<GameObject>();
+	private static string url = "https://api.myjson.com/bins/y73cf";
+
+
+	// Use this for initialization
+	void Start () {
+		WWW www = new WWW(url);
+		StartCoroutine(WaitForRequest(www));
+	}
+
+
+	IEnumerator WaitForRequest(WWW www)
+	{
+		yield return www;
+
+		// check for errors
+		if (www.error == null)
+		{
+			var data = JSON.Parse(www.text);
+			var players = data ["players"];
+			foreach(System.Collections.Generic.KeyValuePair<string,SimpleJSON.JSONNode> kvp in players) {
+				CreatePlayer (kvp.Value ["type"].Value, kvp.Value ["current_player"].AsBool);
+			}
+		} else {
+			Debug.Log("WWW Error: "+ www.error);
+		}    
+	}
+
+	private void CreatePlayer(string type, bool mainPlayer) {
+		string prefabName = "animals/" + type + "/FBX FILES/" + type;
+		GameObject instance = Instantiate(Resources.Load(prefabName, typeof(GameObject))) as GameObject;
+		instance.AddComponent<Player> ();
+		instance.GetComponent<Player> ().playerType = type;
+		instance.GetComponent<Player> ().mainPlayer = mainPlayer;
+		players.Add (instance);
+	}
+}
