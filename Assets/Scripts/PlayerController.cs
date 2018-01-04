ddeaaed1f50c2ea46dc0efa695818ff58b079d96
Nicolas Justiniano <n.justiniano@gmail.com>
2018-01-04 16:29:18 -0300

    all player animations on json

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,23 +12,22 @@ public class PlayerController : MonoBehaviour
 	public Rigidbody rb;
 	private Player player;
 
-	int force = 7;
+	int force = 3;
 	float acceleration = 3.0f;
 
 	void Start() {
 		rb = GetComponent<Rigidbody>();
 		player = this.GetComponent<Player> ();
 
-		rb.mass = 100;
-
 		string animationsData = Read("animations");
 		var data = JSON.Parse(animationsData);
-		var animations = data ["animations"];
+		var animations = data ["playerTypes"];
 		foreach(KeyValuePair<string,SimpleJSON.JSONNode> kvp in animations) {
 			if ( player.playerType == kvp.Value ["type"].Value ) {
 				idleName = kvp.Value ["idle"].Value;
 				walkName = kvp.Value ["walk"].Value;
 				runName = kvp.Value ["run"].Value;
+				rb.mass = kvp.Value ["mass"].AsInt;
 			}
 		}
 		this.transform.localScale = new Vector3 (2, 2, 2);
@@ -42,21 +41,14 @@ public class PlayerController : MonoBehaviour
 	}
 	void FixedUpdate ()
 	{
-		// this.GetComponent<Animation> ().Play (idleName);
 		if (player.mainPlayer == true) {
 			if (Input.GetKeyDown (KeyCode.A)) {
 				rb.velocity += new Vector3(0,force,0) + (acceleration * gameObject.transform.forward);
 				this.GetComponent<Animation> ().Play (runName);
-//
-//
-//				rb.MovePosition (transform.position + transform.up * Time.deltaTime * intensity);
-//				Debug.Log ((transform.position + transform.up * Time.deltaTime * intensity)+"move up");
 			}
 			if (Input.GetKeyDown (KeyCode.Space)) {
 				this.GetComponent<Animation> ().Play (runName);
 				rb.velocity += new Vector3(0,0,force) + (acceleration * gameObject.transform.forward);
-//				rb.MovePosition (transform.position + transform.forward * Time.deltaTime * intensity);
-//				Debug.Log ((transform.position + transform.forward * Time.deltaTime * intensity)+"move forward");
 			}
 		}
 	}
